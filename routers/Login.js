e07c2b542845f09309f058760eb4e1a58b7620a4
dong002 <81304010+dong002@users.noreply.github.com>
2024-01-24 08:36:48 +0900

    :recycle: update deploy.yml

--- routers/Login.js
+++ routers/Login.js
@@ -3,28 +3,30 @@ db.connect();
 
 var express = require('express');
 var router = express.Router();
+exports.router = router;
 
-router.use(express.json());]
+router.use(express.json());
 
-router.post('/login',(req,res)=>{
-    const select = 'select * from employees where empNumber = ?'
-    db.query(select, req.body.empNumber,(err,row,fields)=>{
-        if(err){
-            console.log(err)
-            res.send('login fail')
+router.post('/login', (req, res) => {
+    const select = 'select * from employees where empNumber = ?';
+    db.query(select, req.body.empNumber, (err, row, fields) => {
+        if (err) {
+            console.log(err);
+            res.send('login fail');
         }
-        else{
-            console.log(row)
+        else {
+            console.log(row);
             // console.log(req.body.user_pwd)
-            if(req.body.user_pwd === row[0].password){
-                const param = [req.body.user_id]
-                res.cookie("user",param)
-                res.send('로그인 성공')
+            if (req.body.user_pwd === row[0].password) {
+                const param = [req.body.user_id];
+                res.cookie("user", param);
+                res.send('로그인 성공');
             }
-            else{
-                res.send('wrong password')
+            else {
+                res.send('wrong password');
             }
         }
-    })
-})
+    });
+});
+
 module.exports = router
