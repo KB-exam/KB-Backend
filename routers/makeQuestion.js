b2c6c53c17993265842762f2ce2844957ea0c2ad
dong002 <81304010+dong002@users.noreply.github.com>
2024-01-24 21:55:11 +0900

    :sparkles: create makeQuestion.js

---
+++ routers/makeQuestion.js
@@ -0,0 +1,39 @@
+//DB Connect
+const db = require('../database/conn/db_conn');
+
+//db.connect();
+
+var express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken')
+exports.router = router;
+
+router.use(express.json());
+const insert_4 = 'insert into question (title, content, type, choice1, choice2, choice3, choice4, answer) values (?,?,?,?,?,?,?,?);'
+const insert_2 = 'insert into question (title, content, type, answer) values (?,?,?,?)'
+router.post('/makeQuestion', (req, res) => {
+    if (req.body.type == 2){
+        db.query(insert_2, [req.body.title, req.body.content, req.body.type, req.body.answer], (err, row, fields) => {
+            if (err) {
+                console.log(err);
+                res.send('make fail');
+            }
+            else {
+                res.send('suc OX')
+            }
+        });
+    }
+    else if (req.body.type == 4){
+        db.query(insert_4, [req.body.title, req.body.content, req.body.type, req.body.choice1, req.body.choice2, req.body.choice3, req.body.choice4, req.body.answer], (err, row, fields) => {
+            if (err) {
+                console.log(err);
+                res.send('make fail');
+            }
+            else {
+                res.send('suc 4')
+            }
+        });
+    }
+});
+
+module.exports = router
